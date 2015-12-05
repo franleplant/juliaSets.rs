64f257470136467790d6bf48303da26573e9bb83
Fran Guijarro <franleplant@gmail.com>
2015-12-04 23:47:26 -0300

    working example

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 extern crate image;
 extern crate num;
+extern crate conrod;
+
 use std::fs::File;
 use std::path::Path;
-use std::ops::Add;
 use image::{ImageBuffer, Rgba};
 use num::{Complex};
+use conrod::color::{Color};
 
 static MAX_ITER: i64 = 1000;
 static BAILOUT: f64 = 2.0;
-static N: i64 = 100;
-static M: i64 = 100;
+static N: i64 = 1000;
+static M: i64 = 1000;
+static RANGE_X: f64 = 3.;
+static RANGE_Y: f64 = 3.;
 
 
 fn get_z0(center: &Complex<f64>, range_x: f64, range_y: f64 ) -> Complex<f64> {
@@ -37,13 +41,24 @@ fn fractal(z0: &Complex<f64>, delta: &(f64, f64), width: i64, height: i64) {
 
         let mut k = 0;
         while (z.norm_sqr() < BAILOUT) && (k < MAX_ITER) {
+            //println!("k {}, z {}, norm {}",k, z, z.norm_sqr());
             z = z * z + c;
 
             k += 1;
         }
-        println!("k {}", k);
 
-        Rgba([((k*10) % 255) as u8,255u8,255u8,255u8])
+        // TODO: abstract this in a function so I can test it properly
+        let hue = ((5.0 * (k as f32) + 0.0)% 360.0) * 2.0 * 3.14 / 360.0;
+        //let hue = k as f64 % 360;
+        //println!("k {}, hue {}", k, hue);
+        let color = Color::Hsla(hue as f32, 0.7f32, 0.6f32, 0.9f32);
+        let rgba = color.to_rgb();
+        let r = rgba.0 * 255f32;
+        let g = rgba.1 * 255f32;
+        let b = rgba.2 * 255f32;
+        let a = rgba.3 * 255f32;
+        //println!("{}, {}, {}, {}", r,g,b,a);
+        Rgba([r as u8, g as u8, b as u8, a as u8])
     });
     // Save the image as “fractal.png”
     let ref mut fout = File::create(&Path::new("fractal.png")).unwrap();
@@ -54,10 +69,8 @@ fn fractal(z0: &Complex<f64>, delta: &(f64, f64), width: i64, height: i64) {
 
 fn main() {
     let center = Complex::new(0.0, 0.0);
-    let range_x = 2.;
-    let range_y = 2.;
-    let z0 = get_z0(&center, range_x, range_y);
-    let delta = get_delta(range_x, range_y, N, M);
+    let z0 = get_z0(&center, RANGE_X, RANGE_Y);
+    let delta = get_delta(RANGE_X, RANGE_Y, N, M);
     fractal(&z0, &delta, N, M);
 }
 
